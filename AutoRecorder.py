f40389d7a603d0fd3890a2f9b470ae993a13f428
SMazeikis <salvijus.mazeikis2@mail.dcu.ie>
2020-10-15 21:24:43 -0700

    working and updated readme

--- AutoRecorder.py
+++ AutoRecorder.py
@@ -1,20 +1,17 @@
-import datetime, time, webbrowser, subprocess
-from os import listdir
-from os.path import isfile, join
-import os
+import datetime, time, webbrowser, os
 
-while True: 
-    if datetime.datetime.now().time().hour == 20: 
-        webbrowser.open("google.com")
+while True:
+    # set time for start of meeting hour here
+    if datetime.datetime.now().time().hour == 3: 
+        webbrowser.open("LINK OF MEETING")
         break
     else:
         time.sleep(3600-time.time()%3600)
 
-
 # launches recording for an hour and then kills the obs, saving the zoom recording
-# time.sleep(30)
 bashCommand = 'start /d "C:/Program Files/obs-studio/bin/64bit" obs64.exe --startrecording'
 os.system(bashCommand)
 
+# set the length of meeting in seconds
 time.sleep(30)
 os.system("taskkill /F /IM obs64.exe")
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # AutoRecorder
+This script records a timed set of time of a zoom meeting with Open Broadcast Studio (OBS)
+
+## **prerequisites**
+
+* Install Zoom, log in and ensure:
+    * pop up in browser is set to "always allow"
+    * join call with audio is set automatically
+    * join meeting in full screen is set automatically
+
+* Install OBS and ensure:
+    * OBS filepath is changed or id the same as in script
+    * zoom is selected to be recorded in OBS already
+
+Set zoom link in string and set when and for how long to record the meeting in appropriate places.
+
+**I created this script for myself, for my own use, this is not intended to be misused and there may be unexpected errors.**
